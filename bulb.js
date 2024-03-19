59c410d710aaa2f6bb38c7a1aca2a40a336b8687
karthikmandala1 <163507829+karthikmandala1@users.noreply.github.com>
2024-03-19 09:21:17 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,6 @@
+function bulboff(){
+    document.getElementById("img").src="https://upload.wikimedia.org/wikipedia/commons/8/85/Light-bulb-1.jpg"
+}
+function bulbon(){
+    document.getElementById("img").src="https://cdn.mos.cms.futurecdn.net/HaPnm6P7TZhPQGGUxtDjAg-320-80.jpg"
+}
